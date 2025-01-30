54654920409805c4bf70be6b5f6636bcfce35611
Sumit Tripathi <sumit.tripathi@edmissioncrm.com>
2025-01-30 17:45:21 +0530

    tailwind and routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
-      <h1>HELLO</h1>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
   );
 }
