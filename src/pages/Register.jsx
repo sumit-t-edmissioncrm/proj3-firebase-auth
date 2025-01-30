8746c183b6177f83508bf74aaea9e1ab0bb89ff0
Sumit Tripathi <sumit.tripathi@edmissioncrm.com>
2025-01-30 23:41:49 +0530

    update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -65,7 +65,7 @@ const Register = () => {
                   id="name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Full Name"
-                  required=""
+                  required
                 />
               </div>
 
@@ -88,7 +88,7 @@ const Register = () => {
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="email@example.com"
-                  required=""
+                  required
                 />
               </div>
 
@@ -111,7 +111,7 @@ const Register = () => {
                   id="password"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  required
                 />
               </div>
 
